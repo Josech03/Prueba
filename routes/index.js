f1efea752d9b4802a9d84492e0f0d78f14920680
Josech03 <whitesox3734@gmail.com>
2022-05-28 10:52:48 -0400

    commit

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,6 @@ if (err){
 }
 })
 
-router.get('/', (req, res, next) => {
-  res.render('index.ejs',{data:{},
-  ANALITYCS_KEY:process.env.ANALITYCS_KEY});
-});
 
 const crear="CREATE TABLE IF NOT EXISTS contacts(email VARCHAR(16),nombre VARCHAR(16), comentario TEXT,fecha DATATIME,ip VARCHAR(15));";
 
