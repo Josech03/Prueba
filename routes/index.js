430e51d876efd8b888695a445f0e12e6bc425832
Josech03 <whitesox3734@gmail.com>
2022-06-24 17:28:56 -0400

    m

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ require('dotenv').config();
 
 router.use(express.urlencoded({extended: true}));
 router.use(cookieParser(process.env.GALLETA));
+router.use(cookieSession());
 router.use(session({
 	secret: process.env.GALLETA,
 	resave: true,
